da15e2f9aef4bba3687ab5bfb17373fbc535c504
lwsZZS <lewislzs689809@gmail.com>
2025-08-12 16:27:11 +0000

    MyFirstCommit

--- Main.java
+++ Main.java
@@ -7,21 +7,92 @@ public class Main implements Directions {
   public static void main(String[] args) 
 	{
 		//My robot's name is karel
-    Robot karel = new Robot(5,1,East,9);
+    Robot karel = new Robot(5,1,East,100);
     World.setVisible(true);
     World.setSize(10,10);
     World.setDelay(100);
 
 		karel.move();
+    karel.turnLeft();
+    karel.move();
+    karel.move();
+    karel.move();
+    //initial position
+    karel.turnLeft();
+    karel.turnLeft();
+    karel.move();
     karel.putBeeper();
     karel.move();
+    karel.putBeeper();
     karel.move();
+    karel.putBeeper();
+    karel.move();
+    karel.putBeeper();
+    karel.move();
+    karel.putBeeper();
+    karel.turnLeft();
+    //first part of L
+    karel.move();
+    karel.putBeeper();
+    karel.move();
+    karel.putBeeper();
+    karel.move();
+    //second part of L
+    karel.move();
+    //reposition
+    karel.putBeeper();
+    karel.move();
+    karel.putBeeper();
+    karel.move();
+    karel.putBeeper();
+    karel.move();
+    karel.putBeeper();
+    karel.turnLeft();
+    //
+    karel.move();
+    karel.putBeeper();
+    karel.move();
+    karel.putBeeper();
+    karel.turnLeft();
+    
+    karel.move();
+    karel.putBeeper();
+    karel.move();
+    karel.putBeeper();
+    karel.move();
+    karel.putBeeper();
+    karel.turnLeft();
+    karel.turnLeft();
     karel.turnLeft();
 
     karel.move();
     karel.putBeeper();
+    karel.move();
+    karel.putBeeper();
+    karel.turnLeft();
+    karel.turnLeft();
+    karel.turnLeft();
+
+    karel.move();
+    karel.putBeeper();
+    karel.move();
+    karel.putBeeper();
+    karel.move();
     karel.putBeeper();
 
-  }
+    turnRight(karel); //here is where I call the method
+karel.move();
+karel.move();
     
+  }
+
+//this is a method. We use a placeholder robot as a parameter
+//so thatI can call the method inside of my main method
+
+    public static void turnRight(Robot placeholder){
+      placeholder.turnLeft();
+      placeholder.turnLeft();
+      placeholder.turnLeft();
+}
+
 }
